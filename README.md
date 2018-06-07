ab8e16a8e941ebef62fbc6c5523ba53aad31483e
sebekw <sebekw>
2018-06-07 19:31:16 +0200

    init

---
+++ README.md
@@ -0,0 +1,12 @@
+# users-crud
+
+Used technology: 
+
+Hibernate,
+Spring Boot,
+Spring Data JPA,
+Thymeleaf(template engine),
+H2,
+Maven
+
+address: localhost:8080/
---
+++ src/main/java/com/example/DemoApplication.java
@@ -0,0 +1,17 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@SpringBootApplication
+@EnableTransactionManagement
+@EnableJpaRepositories
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
