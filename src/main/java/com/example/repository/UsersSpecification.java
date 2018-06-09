5f8af6bbb36c62dd36d69e32d49e7e2d7d378da0
Sebastian Wojewoda <swojewoda@o4b.pl>
2018-06-09 18:48:33 +0200

    add predicates

--- src/main/java/com/example/repository/UsersSpecification.java
+++ src/main/java/com/example/repository/UsersSpecification.java
@@ -16,5 +16,32 @@ public class UsersSpecification implements Specification<Users> {
     public UsersSpecification(Users criteria) {
         this.criteria = criteria;
     }
+	
+    @Override
+    public Predicate toPredicate(Root<Users> root, CriteriaQuery<?> criteriaQuery, CriteriaBuilder criteriaBuilder) {
 
+        final List<Predicate> predicates = new ArrayList<>();
+        Path<Object> name = root.get("name");
+        Path<Object> email = root.get("email");
+        Path<Object> lastName = root.get("lastName");
+        Path<Object> pesel = root.get("pesel");
+
+        if (validPredicate(criteria.getName())) {
+            predicates.add(criteriaBuilder.equal(name, criteria.getName()));
+        }
+        if (validPredicate(criteria.getEmail())) {
+            predicates.add(criteriaBuilder.equal(email, criteria.getEmail()));
+        }
+        if (validPredicate(criteria.getLastName())) {
+            predicates.add(criteriaBuilder.equal(lastName, criteria.getLastName()));
+        }
+        if (validPredicate(criteria.getPesel())) {
+            predicates.add(criteriaBuilder.equal(pesel, criteria.getPesel()));
+        }
+        return criteriaBuilder.and(predicates.toArray(new Predicate[predicates.size()]));
+    }
+
+    private boolean validPredicate(String field) {
+        return StringUtils.hasText(field);
+    }
 }
