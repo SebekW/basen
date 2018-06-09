0b2cc33d479c93aad3eefc6c1bd253a8d85921c8
lukasz-skorski <lukasz.aleksander.skorski@gmail.com>
2018-06-09 18:28:50 +0200

    Entity user

--- src/main/java/com/example/entity/Users.java
+++ src/main/java/com/example/entity/Users.java
@@ -1,4 +1,105 @@
 package com.example.entity;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
 public class Users {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "id")
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String lastName;
+
+    @Column
+    private String phone;
+
+    @Column
+    private String email;
+
+    @Column
+    private String pesel;
+
+    private boolean hasActiveTicket;
+
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private Address address;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public Users setId(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Users setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public Users setLastName(String lastName) {
+        this.lastName = lastName;
+        return this;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public Users setPhone(String phone) {
+        this.phone = phone;
+        return this;
+    }
+
+    public String getPesel() {
+        return pesel;
+    }
+
+    public Users setPesel(String pesel) {
+        this.pesel = pesel;
+        return this;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public Users setAddress(Address address) {
+        this.address = address;
+        return this;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Users setEmail(String email) {
+        this.email = email;
+        return this;
+    }
+
+    public boolean isHasActiveTicket() {
+        return hasActiveTicket;
+    }
+
+    public void setHasActiveTicket(boolean hasActiveTicket) {
+        this.hasActiveTicket = hasActiveTicket;
+    }
 }
